d3303545413befce5149eeb5491191b8b0313864
talentdatigan <u07dd18@abdn.ac.uk>
2019-10-15 09:36:13 +0100

    victory

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery_ujs
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+
+import Highcharts from ‘highcharts’;
+
+window.Highcharts = Highcharts;
 
 
 
@@ -16,6 +23,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-$(function () {
-  $('[data-toggle=popover]').popover()
-})
