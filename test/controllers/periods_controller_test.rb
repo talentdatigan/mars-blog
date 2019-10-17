83d42230d630f1edffab30016b221401301b2ee7
talentdatigan <u07dd18@abdn.ac.uk>
2019-10-17 11:37:02 +0100

    diee

--- test/controllers/periods_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PeriodsControllerTest < ActionDispatch::IntegrationTest
-  test "should get measure" do
-    get periods_measure_url
-    assert_response :success
-  end
-
-end
--- test/models/rover_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RoverTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
