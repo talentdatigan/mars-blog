fc0f55efd5681b3f07be4e5169831db9b133621a
Cael <caelmilne2001@gmail.com>
2019-10-17 23:07:13 +0100

    Finished searching function

--- app/controllers/mars_overview_controller.rb
+++ app/controllers/mars_overview_controller.rb
@@ -20,8 +20,23 @@ class MarsOverviewController < ApplicationController
 	end
 
 	def display
-		@year = params[:year]
-		@month = params[:month]
+
+		if params[:year] == nil
+			redirect_to mars_overview_mars_weather_path
+		end
+
+		@year = params[:year].to_i
+		@month = params[:month].to_i
+
+		@weatherdata = []
+		data = Weather.all
+		data.each do |index|
+			if index.year == @year and index.month == @month
+				puts "HERE"
+				@weatherdata << index
+			end
+		end
+
 	end
 
 end
