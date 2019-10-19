ae1e179a52fbb32b54f6b8713930221303cd211c
talentdatigan <u07dd18@abdn.ac.uk>
2019-10-19 11:58:52 +0100

    lecture changes

--- app/controllers/monthly_weathers_controller.rb
+++ app/controllers/monthly_weathers_controller.rb
@@ -12,54 +12,6 @@ class MonthlyWeathersController < ApplicationController
   def show
   end
 
-  # GET /monthly_weathers/new
-  def new
-    @monthly_weather = MonthlyWeather.new
-  end
-
-  # GET /monthly_weathers/1/edit
-  def edit
-  end
-
-  # POST /monthly_weathers
-  # POST /monthly_weathers.json
-  def create
-    @monthly_weather = MonthlyWeather.new(monthly_weather_params)
-
-    respond_to do |format|
-      if @monthly_weather.save
-        format.html { redirect_to @monthly_weather, notice: 'Monthly weather was successfully created.' }
-        format.json { render :show, status: :created, location: @monthly_weather }
-      else
-        format.html { render :new }
-        format.json { render json: @monthly_weather.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /monthly_weathers/1
-  # PATCH/PUT /monthly_weathers/1.json
-  def update
-    respond_to do |format|
-      if @monthly_weather.update(monthly_weather_params)
-        format.html { redirect_to @monthly_weather, notice: 'Monthly weather was successfully updated.' }
-        format.json { render :show, status: :ok, location: @monthly_weather }
-      else
-        format.html { render :edit }
-        format.json { render json: @monthly_weather.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /monthly_weathers/1
-  # DELETE /monthly_weathers/1.json
-  def destroy
-    @monthly_weather.destroy
-    respond_to do |format|
-      format.html { redirect_to monthly_weathers_url, notice: 'Monthly weather was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -9,7 +9,7 @@ class WeathersController < ApplicationController
     @months = (@weathers.distinct.pluck(:month)).sort
 
     if params[:commit] =="Go!"
-      redirect_to mars_overview_display_path(year: params[:search_year], month: params[:search_month])
+      redirect_to mars_overview_display_path(year: params[:search_year], month: params[:search_month]), :target => '_blank'
     end
 
   end
@@ -19,54 +19,7 @@ class WeathersController < ApplicationController
   def show
   end
 
-  # GET /weathers/new
-  def new
-    @weather = Weather.new
-  end
-
-  # GET /weathers/1/edit
-  def edit
-  end
-
-  # POST /weathers
-  # POST /weathers.json
-  def create
-    @weather = Weather.new(weather_params)
-
-    respond_to do |format|
-      if @weather.save
-        format.html { redirect_to @weather, notice: 'Weather was successfully created.' }
-        format.json { render :show, status: :created, location: @weather }
-      else
-        format.html { render :new }
-        format.json { render json: @weather.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /weathers/1
-  # PATCH/PUT /weathers/1.json
-  def update
-    respond_to do |format|
-      if @weather.update(weather_params)
-        format.html { redirect_to @weather, notice: 'Weather was successfully updated.' }
-        format.json { render :show, status: :ok, location: @weather }
-      else
-        format.html { render :edit }
-        format.json { render json: @weather.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /weathers/1
-  # DELETE /weathers/1.json
-  def destroy
-    @weather.destroy
-    respond_to do |format|
-      format.html { redirect_to weathers_url, notice: 'Weather was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
