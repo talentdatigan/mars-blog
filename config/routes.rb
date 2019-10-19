c2d0b74cf38d331f003bf7241b8b23e5f67eab0b
talentdatigan <u07dd18@abdn.ac.uk>
2019-10-19 13:01:38 +0100

    added routing error, changed bootstrap to nav tag

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ get 'mars_overview/mars_weather', to: 'mars_overview#mars_weather'
 get 'mars_overview/insight_mission', to: 'mars_overview#insight_mission'
   get 'mars_overview/display', to: 'mars_overview#display'
 root 'mars_overview#index'
+
+get '*a', :to => 'errors#routing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
