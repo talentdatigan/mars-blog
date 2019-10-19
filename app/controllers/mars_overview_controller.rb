0a95916f3a8e00b4e055921979fb2a995d678035
Cael <caelmilne2001@gmail.com>
2019-10-19 13:12:23 +0100

    Added search re-run function

--- app/controllers/mars_overview_controller.rb
+++ app/controllers/mars_overview_controller.rb
@@ -19,12 +19,27 @@ class MarsOverviewController < ApplicationController
 
 	end
 
+
 	def display
 
+		if params[:commit] == "Run Again!"
+			puts "HERE 4"
+			params[:commit] = nil
+			params[:year] = params[:search_year]
+			params[:month] = params[:search_month]
+
+			#redirect_to mars_overview_display_path(year: params[:search_year], month: params[:search_month]), :target => '_blank'
+			# display?month=1&year=2019
+			#link_to "Show more results", url_for(year: params[:search_year], month: params[:search_month])
+		end
+
+		puts "HERE 1"
 		if params[:year] == nil
+			puts "HERE 2"
 			redirect_to mars_overview_mars_weather_path
 		end
 
+		puts "HERE 3"
 		@year = params[:year].to_i
 		@month = params[:month].to_i
 
@@ -32,10 +47,14 @@ class MarsOverviewController < ApplicationController
 		data = Weather.all
 		data.each do |index|
 			if index.year == @year and index.month == @month
-				puts "HERE"
 				@weatherdata << index
 			end
 		end
+
+		@weathers = Weather.all
+		@years = @weathers.distinct.pluck(:year)
+		@months = (@weathers.distinct.pluck(:month)).sort
+
 
 	end
 
