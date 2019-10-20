82888967fe64462e9284be6581e6a567a0a703f5
Cael <caelmilne2001@gmail.com>
2019-10-20 03:15:28 +0100

    Fixed 1st routing error

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -23,7 +23,11 @@ class WeathersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_weather
+      begin
       @weather = Weather.find(params[:id])
+      rescue
+      redirect_to '/*a'
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
